2879c7cbdb57066083f5b39ad4dfc4004f37d3d2
amanPriyank <amanpriyank2@gmail.com>
2024-08-16 22:57:39 +0530

    Forgot Password Functionality Added

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 const Footer = () => {
   return (
     <div className="footer">
-      <h1 className="text-center">All Right Reserved &copy; PriyankAman</h1>
+      <h1 className="text-center">All Right Reserved &copy; NexShop</h1>
       <p className="text-center mt-3">
-        <Link to="/about">About </Link>|<Link to="/contact">Contact </Link>|
-        <Link to="/policy">Privacy Policy </Link>
+        <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
+        <Link to="/policy">Privacy Policy</Link>
       </p>
     </div>
   );
